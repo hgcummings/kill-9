298387c9124de1720625558cc6c2c2b04eab3848
Harry Cummings <harry@xlr3.com>
2022-08-14 09:07:21 +0100

    Basic rendering of player grid

--- src/client.ts
+++ src/client.ts
@@ -1,4 +1,32 @@
+import { Game } from "./game.js";
 import { KeyboardInput } from "./input.js";
 
 const playerInput = new KeyboardInput();
-playerInput.start(console.log);
+
+const size = 3;
+const game = new Game(size);
+
+function render(cells: Array<Array<number>>) {
+    let playArea = document.getElementById("player-0");
+    if (playArea === null) {
+        return;
+    }
+
+    playArea.innerHTML = "";
+
+    for (let x = 0; x < size; ++x) {
+        for (let y = 0; y < size; ++y) {
+            const value = cells[y][x];
+            if (value !== 0) {
+                const cell = document.createElement("div");
+                cell.style.gridRow = (y + 1).toString();
+                cell.style.gridColumn = (x + 1).toString();
+                cell.innerText = value.toString();
+                cell.classList.add("cell");
+                playArea.appendChild(cell);
+            } 
+        }
+    }
+}
+
+render(game.grid);
--- src/game.ts
+++ src/game.ts
@@ -1,13 +1,13 @@
 export type Direction = [0,-1] | [1,0] | [0,1] | [-1,0]
 
-class Game {
+export class Game {
     grid = Array<Array<number>>()
 
-    constructor(size = 3) {
+    constructor(size: number) {
         for (let y = 0; y < size; ++y) {
             this.grid[y] = new Array<number>();
             for (let x = 0; x < size; ++x) {
-                this.grid[y][x] = 0;
+                this.grid[y][x] = Math.floor(Math.random() * 2.5);
             }
         }
     }
