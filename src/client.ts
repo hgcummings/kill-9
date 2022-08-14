58b454c4de0b87dcbd2c8d0c36a29617c2948a38
Harry Cummings <harry@xlr3.com>
2022-08-14 17:06:52 +0100

    Store game state as array of non-empty cells rather than a grid of values

--- src/client.ts
+++ src/client.ts
@@ -1,4 +1,4 @@
-import { Game } from "./game.js";
+import { Card, Game } from "./game.js";
 import { KeyboardInput } from "./input.js";
 
 const playerInput = new KeyboardInput();
@@ -6,7 +6,7 @@ const playerInput = new KeyboardInput();
 const size = 3;
 const game = new Game(size);
 
-function render(cells: Array<Array<number>>) {
+function render(cards: Array<Card>) {
     let playArea = document.getElementById("player-0");
     if (playArea === null) {
         return;
@@ -14,19 +14,14 @@ function render(cells: Array<Array<number>>) {
 
     playArea.innerHTML = "";
 
-    for (let x = 0; x < size; ++x) {
-        for (let y = 0; y < size; ++y) {
-            const value = cells[y][x];
-            if (value !== 0) {
-                const cell = document.createElement("div");
-                cell.style.gridRow = (y + 1).toString();
-                cell.style.gridColumn = (x + 1).toString();
-                cell.innerText = value.toString();
-                cell.classList.add("cell");
-                playArea.appendChild(cell);
-            } 
-        }
+    for (const card of cards) {
+        const elem = document.createElement("div");
+        elem.style.gridColumn = (card.x + 1).toString();
+        elem.style.gridRow = (card.y + 1).toString();
+        elem.innerText = card.val.toString();
+        elem.classList.add("card");
+        playArea.appendChild(elem);
     }
 }
 
-render(game.grid);
+render(game.cards);
--- src/game.ts
+++ src/game.ts
@@ -1,14 +1,33 @@
 export type Direction = [0,-1] | [1,0] | [0,1] | [-1,0]
 
+export class Card {
+    x: number
+    y: number
+    val: number
+}
+
 export class Game {
-    grid = Array<Array<number>>()
+    cards = Array<Card>()
 
     constructor(size: number) {
-        for (let y = 0; y < size; ++y) {
-            this.grid[y] = new Array<number>();
-            for (let x = 0; x < size; ++x) {
-                this.grid[y][x] = Math.floor(Math.random() * 2.5);
+        while (this.cards.length < size) {
+            const x = Math.floor(Math.random() * size);
+            const y = Math.floor(Math.random() * size);
+
+            if (this.occupied(x, y)) {
+                continue;
             }
+
+            const card = new Card();
+            card.x = x;
+            card.y = y;
+            card.val = 1 + Math.floor(Math.random() * 1.5);
+
+            this.cards.push(card);
         }
+    }
+
+    occupied(x: number, y: number) {
+        return this.cards.some(c => c.x === x && c.y === y);
     }
 }
